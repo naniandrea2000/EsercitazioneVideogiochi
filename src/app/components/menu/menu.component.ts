f278df18a1379d1c085837931424dcc7ad4bcf09
naniandrea2000 <andreanani1400@gmail.com>
2020-03-23 10:39:18 +0100

    Creazione menu 1

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'src/app/models/menu-items.interface';
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+
+  menuList: MenuItem[] = [
+    { id: 1, description: 'Home', selected: true},
+    { id: 2, description: 'Lista giochi', selected: false},
+    { id: 3, description: 'Modifica gioco', selected: false}
+  ]
 
   constructor() { }
 
