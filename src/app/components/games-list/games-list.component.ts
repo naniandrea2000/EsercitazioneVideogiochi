7ab124a29d6779eae1d19a6440a7b84677c25441
naniandrea2000 <andreanani1400@gmail.com>
2020-03-23 12:00:20 +0100

    Implemetazione lista videogiochi

--- src/app/components/games-list/games-list.component.ts
+++ src/app/components/games-list/games-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameItem } from 'src/app/models/game-Items.interface';
 
 @Component({
   selector: 'app-games-list',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesListComponent implements OnInit {
 
+  gamesList: GameItem[] = [
+    {id: 1,nome: "Peggle", descrizione: "colpire il maggior numero di palline lanciando una palla dall'alto dello schermo",genere:"Videogioco rompicapo",rating: 10,prezzo: 5, annoUscita: new Date()},
+    {id: 2,nome: "Fifa 20", descrizione: "Gioco di calcio online e offline",genere:"calcio",rating: 4,prezzo: 55, annoUscita: new Date()},
+    {id: 3,nome: "Rainbow six siege", descrizione: "Gioco strategico multiplayer",genere:"Gioco strategico sparatutto",rating: 9,prezzo: 40, annoUscita: new Date()}
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
