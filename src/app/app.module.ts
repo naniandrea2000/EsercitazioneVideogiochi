f8914b007cea19b92933677f5fe290ff673c5985
naniandrea2000 <andreanani1400@gmail.com>
2020-03-23 10:11:04 +0100

    Creazione components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { GamesListComponent } from './components/games-list/games-list.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { GameItemComponent } from './components/game-item/game-item.component';
+import { EditGameComponent } from './components/edit-game/edit-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    GamesListComponent,
+    GameDetailComponent,
+    GameItemComponent,
+    EditGameComponent
   ],
   imports: [
     BrowserModule
