3a8704d4a9f10d34775809d9de3f74d38f10e974
naniandrea2000 <andreanani1400@gmail.com>
2020-03-30 11:05:39 +0200

    Creazione interfaccia utente

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login(username: string, password: string) {
-  
+    if (username != null && username !== '' && password != null && password !== '') {
+      sessionStorage.setItem('user', username);
+      this.router.navigateByUrl('/home');
+    }
   }
 }
