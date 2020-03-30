77390fa5e83de66dd0bbe1cef019c914b253e056
naniandrea2000 <andreanani1400@gmail.com>
2020-03-30 11:57:14 +0200

    implementazione route gards

--- src/app/services/my-route-gards.service.ts
+++ src/app/services/my-route-gards.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MyRouteGardsService {
+export class MyRouteGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if (sessionStorage.getItem('user') != null) {
+      return true;
+    } else {
+      this.router.navigateByUrl('/login');
+      return false;
+    }
+  }
+
 }
