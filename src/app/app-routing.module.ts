30518656284fdc8f600b5ffce4153085a55a01b7
naniandrea2000 <andreanani1400@gmail.com>
2020-03-30 17:20:41 +0200

    Aggiunta privilegi admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { GamesListComponent } from './components/games-list/games-list.component';
 import { GameDetailComponent } from './components/game-detail/game-detail.component';
-//import { GameItemComponent } from './components/game-item/game-item.component';
 import { EditGameComponent } from './components/edit-game/edit-game.component';
 import { LoginComponent } from './components/login/login.component';
 import { MyRouteGuardService } from './services/my-route-gards.service';
+import { EditRouteGards } from './services/edit-route-gards.service';
 
 const appRoutes: Routes = [
   {path: 'home', component: HomeComponent, canActivate: [MyRouteGuardService]},
   {path: 'list', component: GamesListComponent, canActivate: [MyRouteGuardService]},
-  {path: 'edit', component: EditGameComponent, canActivate: [MyRouteGuardService]},
+  {path: 'edit', component: EditGameComponent, canActivate: [EditRouteGards]},
   {path: 'detail/:id', component: GameDetailComponent, canActivate: [MyRouteGuardService]},
   {path: 'login', component: LoginComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'}
--- src/app/services/edit-route-gards.service.ts
+++ src/app/services/edit-route-gards.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EditRouteGardsService {
+export class EditRouteGards implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if (sessionStorage.getItem('user') != null && sessionStorage.getItem('privilege')=='admin') {
+      return true;
+    } else {
+      this.router.navigateByUrl('/home');
+      return false;
+    }
+  }
+
+  
+
 }
